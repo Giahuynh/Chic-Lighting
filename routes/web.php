e0508417ea1cf48d532ee49d95e76dca0b830bd4
session-1 <baoosake1997@gmail.com>
2025-05-19 19:16:00 +0700

    tét commit

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::post('login', [AuthController::class, 'login']);
 Route::get('register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('register', [AuthController::class, 'register']);
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-
+//fff
 // Public pages
 Route::get('/', fn () => view('home'))->name('home');
 Route::view('/about', 'about')->name('about');
